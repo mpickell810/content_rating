582374059225ccdb709f9cd3b3580c2bc9432589
mpickell810 <silver066surfer@gmail.com>
2026-07-25 18:28:09 -0400

    added like and dislike buttons

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,18 +1,44 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
     super();
+     this.state = {
+      likes: 0,
+      dislikes: 0
+     };
   }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
-}
 
-export default ContentRating;
+    handleLike = () => {
+        this.setState((prevState) => ({
+            likes: prevState.likes + 1
+        }));
+      };
+
+    handleDislike = () => {
+        this.setState((prevState) => ({
+            dislikes: prevState.dislikes + 1
+        }));
+    };
+
+    render() {
+        return (
+          <div className='content-rating'>
+            <p>Text</p>
+            <p>Rate this content below:</p>
+            
+            <div className='rating-buttons'>
+              <button className="like-button" onClick={this.handleLike}>
+                Like ({this.state.likes})
+              </button>
+              <button className="dislike-button" onClick={this.handleDislike}>
+                Dislike ({this.state.dislikes})
+              </button>
+            </div>
+          </div>
+        );
+      }
+    }
+    
+    export default ContentRating;
